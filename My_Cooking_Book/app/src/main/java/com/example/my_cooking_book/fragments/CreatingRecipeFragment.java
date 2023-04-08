1a96d4467152f9fae0f9f784c672cec86e5113d0
gorohovIlya <ilya.gorohov.404@gmail.com>
2023-04-08 19:26:59 +0800

    фрагмент создания рецепта

---
+++ My_Cooking_Book/app/src/main/java/com/example/my_cooking_book/fragments/CreatingRecipeFragment.java
@@ -0,0 +1,22 @@
+package com.example.my_cooking_book.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.my_cooking_book.R;
+
+public class CreatingRecipeFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_creating_recipe, container, false);
+    }
+}
--- My_Cooking_Book/app/src/main/java/com/example/my_cooking_book/fragments/NotesFragment.java
+++ My_Cooking_Book/app/src/main/java/com/example/my_cooking_book/fragments/NotesFragment.java
@@ -1,20 +1,41 @@
 package com.example.my_cooking_book.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.my_cooking_book.R;
 
-public class NotesFragment extends Fragment {
+public class NotesFragment extends Fragment{
+    ListView user_recipes;
+    Button recipe_add;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notes, container, false);
-    }
-}
+        View view = inflater.inflate(R.layout.fragment_notes, null);
+        user_recipes = view.findViewById(R.id.recipe_notes);
+        recipe_add = (Button) view.findViewById(R.id.add_recipe);
+//        return inflater.inflate(R.layout.fragment_notes, container, false);
+        recipe_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                CreatingRecipeFragment creatingRecipeFragment = new CreatingRecipeFragment();
+                transaction.replace(R.id.container, creatingRecipeFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        return view;
+    }}
